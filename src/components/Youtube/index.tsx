ba757ef2f70ef683f027b12cb20dac5569c8c4ac
accuvit <laccuvit@gmail.com>
2020-07-12 18:15:15 -0300

    fixed

--- src/components/Youtube/index.tsx
+++ src/components/Youtube/index.tsx
@@ -1,27 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import './youtube.css';
-import { GoogleApis } from 'googleapis';
 
 import { FaYoutube } from 'react-icons/fa';
-
-import { googleApiKey } from '../../settings.js';
 
 const name = 'anon';
 
 function Youtube() {
-  const google = new GoogleApis();
-  const oauth = new google.auth.OAuth2({
-    clientId: '',
-    clientSecret: '',
-    redirectUri: '',
-  });
-
-  const url = oauth.generateAuthUrl();
   return (
     <div className='youtube-root neumorph-in'>
       <FaYoutube className='icon-youtube' size={100} />
       <p>{!name ? 'User not logged' : `Welcome ${name}!`}</p>
-      <a href={url}>gimme authentication daddy</a>
+      <a href={''}>login(it doesn't work)</a>
     </div>
   );
 }
