261fc64caee0065d4fc938560e3c73a297ebc846
accuvit <laccuvit@gmail.com>
2020-07-12 15:50:08 -0300

    deploy

---
+++ src/settings.js
@@ -0,0 +1,3 @@
+export const spotifyClientId = '33b65e0532a04e0bbac8c3bced1337fc';
+export const sporifyRedirectUri = 'http://localhost:3000';
+export const googleApiKey = 'AIzaSyDz4zsQw78G1PvS-LO4bSgbfrp5KlIX9ik'
