b3242ec0738f9b5e494fde90611c31d45e84aa53
accuvit <laccuvit@gmail.com>
2020-07-13 04:23:11 -0300

    login is ok, but github is having downtime tonight, will deal with oauth response tomorrow

--- src/components/Youtube/index.tsx
+++ src/components/Youtube/index.tsx
@@ -1,16 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './youtube.css';
+import {
+  googleClientId,
+} from '../../settings';
+
+import {
+  GoogleLogin,
+  GoogleLoginResponse,
+  GoogleLoginResponseOffline,
+} from 'react-google-login';
 
 import { FaYoutube } from 'react-icons/fa';
 
-const name = 'anon';
+interface User {
+  googleId: string;
+  imageUrl: string;
+  email: string;
+  name: string;
+  givenName: string;
+  familyName: string;
+}
 
 function Youtube() {
+  const [token, setToken] = useState<string>();
+  const [user, setUser] = useState<User>();
+
+  const successCallback = (
+    res: GoogleLoginResponse | GoogleLoginResponseOffline,
+  ) => {
+    console.log(res);
+  };
+
   return (
     <div className='youtube-root neumorph-in'>
       <FaYoutube className='icon-youtube' size={100} />
-      <p>{!name ? 'User not logged' : `Welcome ${name}!`}</p>
-      <a href={''}>login(it doesn't work)</a>
+      <p>{user ? `You're set, ${user.name}` : 'User not logged'}</p>
+      <GoogleLogin
+        clientId={googleClientId}
+        onSuccess={successCallback}
+        isSignedIn={true}
+      />
     </div>
   );
 }
