57b5d6449e86a70a1c048ee9833056b236dd33f0
accuvit <laccuvit@gmail.com>
2020-07-14 01:39:25 -0300

    YOUTUBE IS ON

--- src/components/Youtube/index.tsx
+++ src/components/Youtube/index.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import './youtube.css';
-import {
-  googleClientId,
-} from '../../settings';
+import { googleClientId } from '../../settings';
 
 import {
   GoogleLogin,
@@ -21,25 +19,34 @@ interface User {
   familyName: string;
 }
 
+const LoginButton = (props: any) => {
+  return <button className={'loginButton neumorph'}>Login Google</button>;
+};
+
 function Youtube() {
   const [token, setToken] = useState<string>();
   const [user, setUser] = useState<User>();
 
+  //Callback from Google login (god I hate so much union types)
   const successCallback = (
     res: GoogleLoginResponse | GoogleLoginResponseOffline,
-  ) => {
-    console.log(res);
+  ): any => {
+    setUser((res as GoogleLoginResponse).profileObj);
+    setToken((res as GoogleLoginResponse).accessToken);
   };
 
   return (
     <div className='youtube-root neumorph-in'>
       <FaYoutube className='icon-youtube' size={100} />
       <p>{user ? `You're set, ${user.name}` : 'User not logged'}</p>
-      <GoogleLogin
-        clientId={googleClientId}
-        onSuccess={successCallback}
-        isSignedIn={true}
-      />
+      {token ? null : (
+        <GoogleLogin
+          clientId={googleClientId}
+          onSuccess={successCallback}
+          isSignedIn={true}
+          render={LoginButton}
+        />
+      )}
     </div>
   );
 }
