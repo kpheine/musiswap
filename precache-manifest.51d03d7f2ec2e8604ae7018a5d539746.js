028bcdba8deafbe0db0c8fb1a8da12188538aa02
accuvit <laccuvit@gmail.com>
2020-07-19 09:03:08 -0300

    Updates

---
+++ precache-manifest.51d03d7f2ec2e8604ae7018a5d539746.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e16db6fba225ae3ddc7a62bffb08fbb",
+    "url": "/musiswap/index.html"
+  },
+  {
+    "revision": "19afa3646c3b6deaac27",
+    "url": "/musiswap/static/css/main.f621b937.chunk.css"
+  },
+  {
+    "revision": "978ef0b56c851801dc18",
+    "url": "/musiswap/static/js/2.4b674bf2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/musiswap/static/js/2.4b674bf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19afa3646c3b6deaac27",
+    "url": "/musiswap/static/js/main.e380f450.chunk.js"
+  },
+  {
+    "revision": "0ac75ac42badb7248d08",
+    "url": "/musiswap/static/js/runtime-main.4757c6d2.js"
+  }
+]);
--- src/settings.js
+++
@@ -1,4 +0,0 @@
-export const spotifyClientId = '33b65e0532a04e0bbac8c3bced1337fc';
-export const sporifyRedirectUri = 'http://localhost:3000';
-export const googleClientId = '230448497881-97p4dipujfsu8i5ehqbbte2rmsl9e1p2.apps.googleusercontent.com'
-export const googleApiKey = 'AIzaSyDz4zsQw78G1PvS-LO4bSgbfrp5KlIX9ik'
