431fa1bbdcc68bd9b196e81faf1de08617c17f67
accuvit <laccuvit@gmail.com>
2020-07-19 10:44:25 -0300

    update

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,4 @@
-export const spotifyClientId = '33b65e0532a04e0bbac8c3bced1337fc';
-export const sporifyRedirectUri = 'http://localhost:3000';
+export const spotifyClientId = '854299fae15c42078e103c1b1f5b6eb7';
+export const sporifyRedirectUri = 'https://accuvit.github.io/musiswap';
 export const googleClientId = '230448497881-97p4dipujfsu8i5ehqbbte2rmsl9e1p2.apps.googleusercontent.com'
 export const googleApiKey = 'AIzaSyDz4zsQw78G1PvS-LO4bSgbfrp5KlIX9ik'
